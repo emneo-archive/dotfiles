63db6af00a5fe128973cacb22964d4b81eb7d055
Alexandre Flion <huntears@kreog.com>
2024-03-30 19:05:11 +0100

    Add pre-commit hook to format

--- flake.nix
+++ flake.nix
@@ -1,10 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs =
     { nixpkgs
+    , nixos-hardware
+    , pre-commit-hooks
     , ...
     }:
     let
@@ -16,8 +26,12 @@
       };
 
       pkgs = (import nixpkgs pkgs-settings);
+
+      mod-hardware = with nixos-hardware.nixosModules; [
+        framework-12th-gen-intel
+      ];
     in
-    {
+    rec {
       formatter.${system} = pkgs.nixpkgs-fmt;
       nixosConfigurations = {
         jiyu = nixpkgs.lib.nixosSystem {
@@ -25,8 +39,20 @@
           modules = [
             ./configuration.nix
             ./hardware-configuration.nix
-          ];
+          ] ++ mod-hardware;
         };
+      };
+      checks.${system}.pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          nixpkgs-fmt = {
+            enable = true;
+            name = pkgs.lib.mkForce "Nix files format";
+          };
+        };
+      };
+      devShells.${system}.default = pkgs.mkShell {
+        inherit (checks.${system}.pre-commit-check) shellHook;
       };
     };
 
