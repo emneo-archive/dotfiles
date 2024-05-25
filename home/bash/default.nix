1b101d2b48a360d0bdf486178d5270b5ae58201e
huntears <huntears@kreog.com>
2024-05-25 13:58:06 +0200

    Remove bash wakatime integration

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -1,20 +1,9 @@
-{ pkgs, ... }:
-let
-  bash-wakatime = pkgs.fetchFromGitHub {
-    owner = "gjsheep";
-    repo = "bash-wakatime";
-    rev = "c97292398936393c3f985f4924a3c234793ca3b8";
-    sha256 = "sha256-Heq/VxfCqFhnYxAm2ejymANdPmZ5uNixuZiuC/53VQE=";
-  };
-in
+{ ... }:
 {
   programs.bash = {
     enable = true;
 
     initExtra =
-      (builtins.readFile ./.bashrc)
-      + ''
-        . ${bash-wakatime}/bash-wakatime.sh
-      '';
+      (builtins.readFile ./.bashrc);
   };
 }
